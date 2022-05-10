664741ccf684742d215153d015b4f8f8935a56d0
Aung Myint Myat <aungmyat23711@gmail.com>
2022-05-10 14:47:56 +0630

    backend

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
+app.get('/', (res, req) => {
+  res.setEncoding('Server is working now');
+});
 
 const PORT = process.env.PORT || 5000;
 
