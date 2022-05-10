fda3b1a5c4ad95e56b524f008b2b4134e7123d32
Aung Myint Myat <aungmyat23711@gmail.com>
2022-05-10 14:50:05 +0630

    backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 app.get('/', (res, req) => {
-  res.setEncoding('Server is working now');
+  res.send('Server is working now');
 });
 
 const PORT = process.env.PORT || 5000;
